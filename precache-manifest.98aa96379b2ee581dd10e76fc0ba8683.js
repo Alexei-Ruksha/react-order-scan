7189ef5bde5e501c2487ad80edce462fe84330c5
Alexei Ruksha <alexei.ruksha@intexsoft.by>
2019-02-23 23:07:57 +0300

    Updates

---
+++ precache-manifest.98aa96379b2ee581dd10e76fc0ba8683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-order-scan/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0c834b7af71e49208746",
+    "url": "/react-order-scan/static/js/runtime~main.0c834b7a.js"
+  },
+  {
+    "revision": "a0e2c9ccb4d3b3f1d838",
+    "url": "/react-order-scan/static/js/main.a0e2c9cc.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/react-order-scan/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "a0e2c9ccb4d3b3f1d838",
+    "url": "/react-order-scan/static/css/main.38ff8b23.chunk.css"
+  },
+  {
+    "revision": "b341168df4dec2532ad9eb29e5a874d8",
+    "url": "/react-order-scan/index.html"
+  }
+];
